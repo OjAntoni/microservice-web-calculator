47833f815aca45318e1f0b233d857ce1af0ccab3
OjAntoni <zezyulin90@gmail.com>
2022-02-21 17:55:42 +0300

    Adding all basic classes and tests

---
+++ src/test/java/by/innowise/monolithwebcalculator/MonolithWebCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package by.innowise.monolithwebcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithWebCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
