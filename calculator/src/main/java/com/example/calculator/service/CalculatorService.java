b34456cd879d7d22f1db08377a6b913a8b62ad86
OjAntoni <zezyulin90@gmail.com>
2022-05-05 22:41:55 +0300

    SD (Discovery client) + precision for division in KV Consul

--- calculator/src/main/java/com/example/calculator/service/CalculatorService.java
+++ calculator/src/main/java/com/example/calculator/service/CalculatorService.java
@@ -1,52 +1,62 @@
 package com.example.calculator.service;
 
 import com.example.calculator.aop.annotation.Persisting;
-import com.example.calculator.util.properties.MicroserviceNetworkProperties;
 import com.example.commonlogic.domain.mapper.OperationMapper;
 import com.example.commonlogic.domain.operation.Operation;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.List;
+
 @Service
 public class CalculatorService {
     private final OperationMapper mapper;
-    private final MicroserviceNetworkProperties prop;
+    private final DiscoveryClient discoveryClient;
 
-    public CalculatorService(OperationMapper mapper, MicroserviceNetworkProperties prop) {
+    public CalculatorService(OperationMapper mapper, DiscoveryClient discoveryClient) {
         this.mapper = mapper;
-        this.prop = prop;
+        this.discoveryClient = discoveryClient;
     }
 
     @Persisting
     public Operation requestAndGetOperation(Operation op, String type){
         String path = "";
-        String uri = "";
+        String method = "";
         switch (type) {
             case "add":
-                path += prop.getADD_SERVICE_URI();
-                System.out.println(path);
-                uri = "/add";
+                path += serviceUrl("add-service");
+                method = "/add";
                 break;
             case "sub":
-                path += prop.getSUB_SERVICE_URI();
-                uri = "/sub";
+                path += serviceUrl("sub-service");
+                method = "/sub";
                 break;
             case "mult":
-                path += prop.getMULT_SERVICE_URI();
-                uri = "/mult";
+                path += serviceUrl("mult-service");
+                method = "/mult";
                 break;
             case "div":
-                path += prop.getDIV_SERVICE_URI();
-                uri = "/div";
+                path += serviceUrl("div-service");
+                method = "/div";
                 break;
         }
         WebClient webClient = WebClient.create(path);
-        return webClient.post().uri(uri).accept(MediaType.APPLICATION_JSON)
+        return webClient.post().uri(method).accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(mapper.mapOperationToOperationRequestDto(op))
                 .retrieve()
                 .bodyToMono(Operation.class)
                 .block();
+    }
+
+    private String serviceUrl(String serviceId) {
+        List<ServiceInstance> list = discoveryClient.getInstances(serviceId);
+        if (list != null && list.size() > 0 ) {
+            return list.get(0).getUri().toString();
+        }
+        return null;
     }
 }
--- div-microservice/src/main/java/com/example/divmicroservice/service/DivisionService.java
+++ div-microservice/src/main/java/com/example/divmicroservice/service/DivisionService.java
@@ -2,16 +2,29 @@ package com.example.divmicroservice.service;
 
 import com.example.commonlogic.domain.operation.Operation;
 import com.example.commonlogic.domain.operation.OperationType;
+import com.example.divmicroservice.util.properties.DivisionProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Service
 public class DivisionService {
+    private final DivisionProperties prop;
+
+    public DivisionService(DivisionProperties prop) {
+        this.prop = prop;
+    }
 
     public Operation calc(Operation operation){
+        double res = BigDecimal.valueOf(operation.getArgTwo()!=0 ? operation.getArgOne()/operation.getArgTwo() : Double.MAX_VALUE).
+                setScale(prop.getPrecision(), RoundingMode.HALF_UP).doubleValue();
         return Operation.builder()
                 .argTwo(operation.getArgTwo())
                 .argOne(operation.getArgOne())
-                .result(operation.getArgTwo()!=0 ? operation.getArgOne()/operation.getArgTwo() : Double.MAX_VALUE)
+                .result(res)
                 .operationType(OperationType.DIVIDE)
                 .build();
     }
